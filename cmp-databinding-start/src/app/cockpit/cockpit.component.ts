e97cf12021ddcac73e5f6fe18710046eb9b81c85
Joshua Rippon <joshuarippon1@gmail.com>
2018-11-06 18:31:59 +1300

    Initial Commit

---
+++ cmp-databinding-start/src/app/cockpit/cockpit.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit, EventEmitter, Output } from "@angular/core";
+
+@Component({
+  selector: "app-cockpit",
+  templateUrl: "./cockpit.component.html",
+  styleUrls: ["./cockpit.component.css"]
+})
+export class CockpitComponent implements OnInit {
+  @Output()
+  serverCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+
+  @Output()
+  blueprintCreated = new EventEmitter<{
+    serverName: string;
+    serverContent: string;
+  }>();
+  newServerContent = "";
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  onAddServer(nameInput: HTMLInputElement) {
+    console.log(nameInput);
+    this.serverCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint(nameInput: HTMLInputElement) {
+    this.blueprintCreated.emit({
+      serverName: nameInput.value,
+      serverContent: this.newServerContent
+    });
+  }
+}
